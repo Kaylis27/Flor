3cda08f9823867008af4ffb013c35aa2284ee8bb
Kaylis27 <zerodragonds@gmail.com>
2026-05-30 17:35:34 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-onload = () => {
-    setTimeout(() => {
-        document.body.classList.remove("container");
-    }, 18000); // 18000 milisegundos = 18 segundos de espera
+onload = () =>{
+    document.body.classList.remove("container");
 };
